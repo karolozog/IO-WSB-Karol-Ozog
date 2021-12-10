b15b8191def97c77e4e7f6e4630cdb37f5819d01
karolozog <62423032+karolozog@users.noreply.github.com>
2021-12-10 13:59:29 +0100

    Add files via upload

--- zad32.py
+++ zad32.py
@@ -1,8 +1,8 @@
-x = 19
+x = int(input("podaj liczbę: "))
 
 for i in range(2, x):
-    if x % 2 == 0:
-        print(x, "nie jest liczbą pirwszą")
+    if x % i == 0:
+        print(x, "nie jest liczbą pierwszą")
         break
     else:
         print(x, "jest liczbą pierwszą")
