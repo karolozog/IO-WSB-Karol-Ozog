c4925a16e84c008f4b61c6489b4a256b87b2b761
karolozog <62423032+karolozog@users.noreply.github.com>
2021-12-10 14:16:55 +0100

    Update zad38.py

--- zad38.py
+++ zad38.py
@@ -1,6 +1,4 @@
-import string    
-import random 
-print("podaj liczbę dodatnią:") 
-losowe = (int(input()))
-ran = ''.join(random.choices(string.ascii_uppercase + string.digits, k = losowe))    
-print("Wygenerowany string to : " + str(ran)) 
+x = int(input("podaj liczbę: "))
+
+print(x*"X")
+    
