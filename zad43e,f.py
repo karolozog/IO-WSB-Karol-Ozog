9450641e39367d4ed7f06214e86b288b44ac1c12
karolozog <62423032+karolozog@users.noreply.github.com>
2021-12-11 00:34:46 +0100

    Add files via upload

---
+++ zad43e,f.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+wektor = np.random.randint(100, size=(50))
+
+srednia = np.average(wektor)
+minimalna = np.min(wektor)
+maksymalna = np.max(wektor)
+odchylenie = np.std(wektor)
+
+print(wektor)
+print("średnia liczba całkowita to: ", int(srednia))
+print("najmniejsza liczba to: ",minimalna)
+print("największa liczba to: ",maksymalna)
+print("odchylenie standardowe wynosi: ", int(odchylenie))
