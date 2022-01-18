d62fdfbe825be4b38d3d1281847ddc9cdc329ccd
karolozog <62423032+karolozog@users.noreply.github.com>
2022-01-18 21:37:17 +0100

    Add files via upload

---
+++ lab2/zad2.py
@@ -0,0 +1,36 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import tree
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix
+
+df = pd.read_csv("iris.csv")
+
+#numeryczne kolumny
+all_inputs = df[["sepal.length", "sepal.width", "petal.length", "petal.width"]].values
+#kolumna z gatunkiem
+all_classes =  df["variety"].values
+
+#podział na zbiory testowy i treningowy oraz na klasę numeryczną
+(train_inputs, test_inputs, train_classes, test_classes) = train_test_split(all_inputs, all_classes, train_size = 0.7, random_state = 1)
+
+#tworzenie drzewa i wygenerowanie
+dtc = DecisionTreeClassifier()
+dtc.fit(train_inputs, train_classes)
+
+#wyświetlenie drzewa i zapisanie w pliku eps
+plt.figure()
+tree.plot_tree(dtc)
+plt.savefig("tree.eps", format = "eps", bbox_inches = "tight")
+
+#dokładność modelu na zbiorze testowym
+dokladnosc = dtc.score(test_inputs, test_classes)
+print(dokladnosc)
+
+#predict pokazuje odgadnięte prawidłowo nazy irysów
+test_zgadniete = dtc.predict(test_inputs)
+
+#macierz błędu
+cf = confusion_matrix(test_classes, test_zgadniete)
+print(cf)
