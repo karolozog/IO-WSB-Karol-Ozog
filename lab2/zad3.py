16b555aa164358f8d1f4861ee0507fd9f88bf2d6
karolozog <62423032+karolozog@users.noreply.github.com>
2022-01-24 17:45:03 +0100

    Update zad3.py

--- lab2/zad3.py
+++ lab2/zad3.py
@@ -3,10 +3,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.metrics import confusion_matrix
 
-    #odległość eukialidedowa
+# odległość eukialidedowa
 def euclidean_distance(x1, x2):
     return np.sqrt(np.sum((x1 - x2) ** 2))
-
 
 class KNN:
     def __init__(self, k=3):
@@ -19,6 +18,7 @@ class KNN:
     def predict(self, X):
         y_pred = [self._predict(x) for x in X]
         return np.array(y_pred)
+
     def _predict(self, x):
         # Obliczanie dystansu pomiędzy punktem x a resztą zbioru treningowego
         distances = [euclidean_distance(x, x_train) for x_train in self.X_train]
@@ -30,16 +30,16 @@ class KNN:
         most_common = Counter(k_neighbor_labels).most_common(1)
         return most_common[0][0]
 
-
 if __name__ == "__main__":
     # Imports
     from matplotlib.colors import ListedColormap
     from sklearn import datasets
     from sklearn.model_selection import train_test_split
-    #kolory na wykresie
+
+    # kolory na wykresie
     cmap = ListedColormap(["#FF0000", "#00FF00", "#0000FF"])
 
-    #dokładność
+    # dokładność
     def accuracy(y_true, y_pred):
         accuracy = np.sum(y_true == y_pred) / len(y_true)
         return accuracy
@@ -49,29 +49,31 @@ if __name__ == "__main__":
 
     # podział na zbiory testowy i treningowy oraz na klasę numeryczną
     X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.7, random_state=1
+        X, y, test_size=0.67, random_state=1
     )
-     
-      #określanie ilości "najbliższych sąssiadów k= 3"
-    k = 3
+
+    # określanie ilości "najbliższych sąssiadów k= 3. Żeby otrzymać metodę k-NN dla 5 lub 11 sąsiadów za k podstawiamy kolejno 5 lub 11 "
+    k = 11
     clf = KNN(k=k)
     clf.fit(X_train, y_train)
     predictions = clf.predict(X_test)
     print("Klasyfikacja KNN przy", k, "najbliższych sąsiadach wynosi: ", accuracy(y_test, predictions), "%")
 
-    # określanie ilości "najbliższych sąssiadów k = 5"
-    i = 5
-    clf = KNN(k=k)
-    clf.fit(X_train, y_train)
-    predictions = clf.predict(X_test)
-    print("Klasyfikacja KNN przy", i, "najbliższych sąsiadach wynosi: ", accuracy(y_test, predictions), "%")
+    # macierz błędu
+    cf = confusion_matrix(y_test, predictions)
+    print("macierz błędu to:")
+    print(cf)
 
-    # określanie ilości "najbliższych sąssiadów k = 11"
-    j = 11
-    clf = KNN(k=k)
-    clf.fit(X_train, y_train)
-    predictions = clf.predict(X_test)
-    print("Klasyfikacja KNN przy", j, "najbliższych sąsiadach wynosi: ", accuracy(y_test, predictions), "%")
+    # wykres
+    plt.figure()
+    plt.scatter(X[:, 0], X[:, 1], c=y, cmap=cmap, edgecolor="k", s=20)
+    # oś x
+    plt.xlabel('długość ')
+    # oś y
+    plt.ylabel('szerokość')
+    # tytuł
+    plt.title('Klasyfikacja KNN Irysów')
+    plt.show()
 
 
     # macierz błędu
